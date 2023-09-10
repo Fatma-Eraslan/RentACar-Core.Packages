6ef305e893c435dea79bf2dcd088548581b037a3
Fatma-Eraslan <fatoseraslan275@gmail.com>
2023-09-10 19:15:21 +0300

    Pagination

---
+++ Core.Persistence/Paging/Paginate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Paging
+{
+    public class Paginate<T>
+    {
+        public Paginate()
+        {
+            
+        }
+
+        public int Size { get; set; }//data sayısı
+        public int Index { get; set; }//hangi sayfa
+        public int Count { get; set; }//kayıt sayısı
+        public int Pages { get; set; }//Sayfa sayısı
+        public IList<T> Items { get; set; }
+        public bool HasPrevious => Index > 0;//önceki sayfa var mı
+        public bool HasNext => Index+1 > Pages;//sonraki sayfa var mı
+    }
+}
