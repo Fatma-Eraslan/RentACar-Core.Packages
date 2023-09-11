4807cc3101d82246105df8d270efdf9ee548a83f
Fatma-Eraslan <fatoseraslan275@gmail.com>
2023-09-11 22:02:16 +0300

    Entity Framework implementation

--- Core.Persistence/Repositories/IRepository.cs
+++ Core.Persistence/Repositories/IRepository.cs
@@ -8,59 +8,43 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Persistence.Repositories
+namespace Core.Persistence.Repositories;
+
+public interface IRepository<TEntity, TEntityId> : IQuery<TEntity>
+    where TEntity : Entity<TEntityId>
 {
-    public interface IRepository<TEntity, TEntityId>:IQuery<TEntity>
-        where TEntity : Entity<TEntityId>
-    {
-        TEntity? Get(
-            Expression<Func<TEntity, bool>> predicate,
-            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
-            bool withDeleted = false,//silinenleri listlesin mi, listelemesin mi
-            bool enableTracking = true,//ef nin izleme desteğinin(tracking) enable edilip edilmeyeceği
-            CancellationToken cancellationToken = default//iptal etme işlemi            
-         );
+    TEntity? Get(
+        Expression<Func<TEntity, bool>> predicate,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        bool withDeleted = false,
+        bool enableTracking = true
+    );
 
-        Paginate<TEntity?> GetList(
-               Expression<Func<TEntity, bool>> predicate = null,
-               Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
-               Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
-               int index = 0,
-               int size = 10, //sayfalama 
-               bool withDeleted = false,
-               bool enableTracking = true,
-               CancellationToken cancellationToken = default
-            );
+    Paginate<TEntity> GetList(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool withDeleted = false,
+        bool enableTracking = true
+    );
 
-        Paginate<TEntity?> GetListBydynamic(
-            DynamicQuery dynamic,
-               Expression<Func<TEntity, bool>> predicate = null,
-               Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
-               Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
-               int index = 0,
-               int size = 10, //sayfalama 
-               bool withDeleted = false,
-               bool enableTracking = true,
-               CancellationToken cancellationToken = default
-            );
+    Paginate<TEntity> GetListByDynamic(
+        DynamicQuery dynamic,
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool withDeleted = false,
+        bool enableTracking = true
+    );
 
-        bool Any( //aradığımız veri var mı yok mu
-              Expression<Func<TEntity, bool>> predicate = null,
-               bool withDeleted = false,
-               bool enableTracking = true,
-               CancellationToken cancellationToken = default
-            );
-
-        TEntity Add(TEntity entity);
-
-        ICollection<TEntity> AddRange(ICollection<TEntity> entitiy);
-
-        TEntity Update(TEntity entity);
-
-        ICollection<TEntity> UpdateRange(ICollection<TEntity> entitiy);//çoklu
-
-        TEntity Delete(TEntity entity, bool permanent = false);//kalıcı mı sileyim soft mu sileyim
-
-        TEntity DeleteRange(ICollection<TEntity> entitiy, bool permanent = false);
-    }
+    bool Any(Expression<Func<TEntity, bool>>? predicate = null, bool withDeleted = false, bool enableTracking = true);
+    TEntity Add(TEntity entity);
+    ICollection<TEntity> AddRange(ICollection<TEntity> entities);
+    TEntity Update(TEntity entity);
+    ICollection<TEntity> UpdateRange(ICollection<TEntity> entities);
+    TEntity Delete(TEntity entity, bool permanent = false);
+    ICollection<TEntity> DeleteRange(ICollection<TEntity> entity, bool permanent = false);
 }
