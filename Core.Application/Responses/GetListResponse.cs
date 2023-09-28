b79bbd10028e97fef72d55ec7063cc83cbbf29eb
Fatma-Eraslan <fatoseraslan275@gmail.com>
2023-09-28 22:00:51 +0300

    save

---
+++ Core.Application/Responses/GetListResponse.cs
@@ -0,0 +1,20 @@
+﻿using Core.Persistence.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Responses
+{
+    public class GetListResponse<T>: BasePageableModel
+    {
+        private IList<T> _item;
+
+        public IList<T> Item 
+        { 
+            get => _item??=new List<T>();//itemler yoksa list oluştur
+            set => _item = value; 
+        }
+    }
+}
---
+++ Core.Persistence/Paging/BasePageableModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Paging
+{
+    public abstract class BasePageableModel
+    {
+        public int Size { get; set; }//data sayısı
+        public int Index { get; set; }//hangi sayfa
+        public int Count { get; set; }//kayıt sayısı
+        public int Pages { get; set; }//Sayfa sayısı
+        public bool HasPrevious { get; set; }
+        public bool HasNext { get; set; }
+    }
+}
