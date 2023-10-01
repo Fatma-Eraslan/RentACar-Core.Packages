a71541e0f0098698adaee53eeeb3c828e2d8af85
Fatma-Eraslan <fatoseraslan275@gmail.com>
2023-10-01 16:09:58 +0300

    Validation Application with Mediatr Pipeline

--- Core.CrossCuttingConcerns/Exceptions/Handlers/HttpExceptionHandler.cs
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/HttpExceptionHandler.cs
@@ -2,12 +2,15 @@
 using Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
 using Core.CrossCuttingConcerns.Exceptions.Types;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails.BusinessProblemDetails;
+//using static Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails.BusinessProblemDetails;
+using ValidationProblemDetails = Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails.ValidationProblemDetails;
 
 namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
 
@@ -32,6 +35,14 @@ public class HttpExceptionHandler : ExceptionHandler
     {
         Response.StatusCode = StatusCodes.Status500InternalServerError;
         string details = new InternalServerErrorProblemDetails(exception.Message).AsJson();
+        return Response.WriteAsync(details);
+    }
+
+    protected override Task HandleException(ValidationException validationException)
+    {
+
+        Response.StatusCode = StatusCodes.Status400BadRequest;
+        string details = new ValidationProblemDetails(validationException.Errors).AsJson();
         return Response.WriteAsync(details);
     }
 }
