1f5479a71b61adb7fb23129edf21f6bba14206af
Fatma-Eraslan <fatoseraslan275@gmail.com>
2024-10-30 21:11:50 +0300

    Creating RefreshToken OTP and Email Authenticator Assets

---
+++ Core.Security/Entities/RefreshToken.cs
@@ -0,0 +1,45 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.Entities;
+
+public class RefreshToken:Entity<int>//access token(jwt ile sisteme başarılı girmesini sağlayan token) ın süresi dolmuşsa veya yeniden alınması gerektiğinde kullanılan anahtar(refresh token)
+{
+    public int UserId { get; set; }
+    public string Token { get; set; }
+    public DateTime Expires { get; set; }
+    public string CreatedByIp { get; set; }
+    public DateTime? Revoked { get; set; }//token iptal edilme süresi
+    public string? RevokedByIp { get; set; }
+    public string? ReplacedByToken { get; set; }
+    public string? ReasonRevoked { get; set; }
+
+    public virtual User User { get; set; } = null!;
+
+    public RefreshToken()
+    {
+        Token = string.Empty;
+        CreatedByIp = string.Empty;
+    }
+
+    public RefreshToken(int userId, string token, DateTime expires, string createdByIp)
+    {
+        UserId = userId;
+        Token = token;
+        Expires = expires;
+        CreatedByIp = createdByIp;
+    }
+
+    public RefreshToken(int id, int userId, string token, DateTime expires, string createdByIp)
+        : base(id)
+    {
+        UserId = userId;
+        Token = token;
+        Expires = expires;
+        CreatedByIp = createdByIp;
+    }
+}
